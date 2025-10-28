2aa33f96dfa93b882df76a574b329e381a9e650a
Avi Fisher <avifisher82@gmail.com>
2025-10-28 11:41:17 +0200

    creat a new function 'creat_tabel_card' and 'mixing'

--- memory_game/back.py
+++ memory_game/back.py
@@ -1,5 +1,7 @@
+import random
+from random import randint
 
-def creat_table(size):
+def creat_table_card(size):
     matrix = []
     count = 0
 
@@ -23,12 +25,27 @@ def check_2(num):
         else:
             return False
 
-def creat_table_card(size):
+def creat_table(size):
     matrix_card = []
 
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append("*")
+
+        matrix_card.append(row)
     return matrix_card
 
 def mixing(tabel):
+    size = len(tabel) - 1
+
+    for i in range(300):
+        random1 = random.randint(0, size)
+        random2 = random.randint(0, size)
+        random3 = random.randint(0, size)
+        random4 = random.randint(0, size)
+
+        tabel[random1][random2],tabel[random3][random4] =  tabel[random3][random4],tabel[random1][random2]
 
     return tabel
 
